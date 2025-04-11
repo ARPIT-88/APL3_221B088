62289b2d9660c82d54c93cc7fcbbc1bee88b0129
221B088 <221B088@juet.co.in>
2025-04-11 09:27:15 +0530

    lab6

---
+++ LE6/Test.java
@@ -0,0 +1,15 @@
+public class Test
+{
+    public static void main(String[] args) {
+        Offering offering = new Coffee ();
+        offering = new Rum (offering);
+        offering = new Rum (offering);
+        System.out.println( offering .getName() + " " + offering.getPrice());
+        
+       
+        Offering offering2 = new Tea ();
+        offering2 = new Rum (offering2);
+        offering2 = new Rum (offering2);
+        System.out.println( offering2 .getName() + " " + offering2.getPrice());
+    }
+}
